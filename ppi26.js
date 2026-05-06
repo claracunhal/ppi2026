71aa1ae28c2515d5b425fa17d7613cd62049a982
Clara Cunha <clara.cunha1@escolar.ifrn.edu.br>
2026-05-06 19:19:22 +0000

    Aula 06/05 - Introdução a Javascript

---
+++ ppi26.js
@@ -0,0 +1,34 @@
+/*
+Programação para internet - aula 06/05
+   - Introdução ao Javascript 
+*/
+
+console.log("Welcome to ppi");
+
+//Variáveis
+let name = "João";
+let age = 17;
+let isStudent = true;
+
+const PI = 3.14; //Não pode mudar o valor
+console.log(PI);
+
+// Operadores Aritméticos + - / * ** %
+let x = 5 + 5; //soma
+let y = "5" + 5; // concatenação
+let z = "hello" + 5;
+
+console.log(x, y, z);
+console.log(typeof x);
+
+//Ativar formatador prettier (alt + shift + f)
+
+// Operadores relacionais > < >= <= != === !==
+console.log(5 != "5"); //diferença
+console.log(5 == "5"); //comparação valor
+console.log("5" == 5); //comparação de valor ou tipo
+console.log("5" === 5); //comparação de valor e tipo
+
+// Operadores de incremento 
+console.log(x++);
+console.log(x);
