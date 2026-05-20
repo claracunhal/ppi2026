b3d891ca89c7dfdb1c879e6f79367510d0461e50
Clara Cunha <clara.cunha1@escolar.ifrn.edu.br>
2026-05-20 19:19:21 +0000

    Aula - 20/05/26

--- ppi26.js
+++ ppi26.js
@@ -170,3 +170,124 @@ console.log(numbers.toSorted((a, b) => b-a)); //D
 
 numbers.sort((a, b) => a-b)//maior valor
 
+console.log('Maior =', numbers[numbers.length-1]);//menor valor
+
+console.log('Menor =', numbers[0]);
+console.log("Menor =", numbers[0]);
+
+// **********************************
+// Aula 13/05 - Funções, array destructuring, spread operator
+console.log(fruits);
+
+const fruits2 = ["Kiwi", "Avocado"];
+console.log([...fruits2, "Grape"]);
+
+// Spread - ...
+const fruits3 = [...fruits, ...fruits2];
+console.log(fruits3);
+
+// DESAFIO - Exibir todas as frutas que comecem
+// com a letra A
+const out = [];
+const letra = "a";
+
+// for (let i = 0; i < fruits3.length; i++) {
+//   const fruit = fruits3[i];
+//   //toLowerCase - minúscula, toUpperCase - maiúscula
+//   if (fruit[0].toLowerCase() === letra) {
+//     out.push(fruit);
+//   }
+// }
+
+// fruits3.map((fruit) => {
+//   if (fruit[0].toLowerCase() === letra) {
+//     out.push(fruit);
+//   }
+// });
+
+// find - retorna a primeira ocorrência,
+// de acordo com a condição
+let outFind = fruits3.find(
+  (fruit) => fruit[0].toLowerCase() === letra
+);
+console.log(outFind);
+
+// filter - retorna TODAS as ocorrências,
+// de acordo com a condição
+let outFilter = fruits3.filter(
+  (fruit) => fruit[0].toLowerCase() === letra
+);
+console.log(outFilter);
+
+// DESAFIO - Exibir o valor da soma de todos os
+// números do array 'numbers'
+
+console.log(numbers);
+
+// map
+let soma = 0;
+numbers.map((number) => {
+  soma += number;
+});
+console.log(soma);
+
+// reduce
+console.log(
+  numbers.reduce((soma, number) => soma + number)
+);
+
+// Object
+const pessoa = {
+  nome: 'Zé Vaqueiro',
+  idade: 25,
+  profissao: 'Cantor/Compositor'
+};
+
+console.log(pessoa.nome);
+console.log(pessoa['nome']);
+
+console.log(pessoa['nome']);
+console.log(Object.keys(pessoa));
+console.log(Object.values(pessoa));
+
+const pessoa2 = { ...pessoa, hobby: 'Vaquejada'};
+console.log(pessoa2);
+
+// Atribuição por desestruturação
+const { nome, idade } = pessoa2;
+console.log(nome, idade);
+
+// Construtor - Protótipo de Objeto
+function Product(name, sto, ram, price, qty, category){
+  this.name = name;
+  this.sto = sto;
+  this.ram = ram;
+  this.price = price;
+  this.qty = qty;
+  this.category = category;
+}
+
+const p1 = new Product('iPhone', 256, 16, 5000, 10, 'Celular');
+const p2 = new Product('iPad', 512, 16, 8000, 20, 'Tablet');
+const p3 = new Product('Macbook', 1024, 24, 15000, 30, 'Notebook');
+const p4 = new Product('iPhone Pro Max', 256, 16, 7000, 15, 'Celular');
+const p5 = new Product('iPad Pro', 512, 16, 10000, 25, 'Tablet');
+const p6 = new Product('Macbook Pro', 1024, 24, 25000, 35, 'Notebook');
+
+const estoque = [ p1, p2, p3, p4, p5, p6];
+console.log(estoque);
+
+/* DESAFIO 1
+
+  a) Valor total de todos os produtos em estoque
+  b) Ordene os produtos por nome (crescente/descrescente)
+  c) Ordene os produtos por preço (crescente/descrescente)
+  d) Filtre produtos de acordo uma categoria
+
+*/
+
+//a)//
+const ValorTotal = estoque
+   .reduce((valor, p) => valor + p.price * p.qty, 0)
+
+
