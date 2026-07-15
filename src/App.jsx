fe6cc768663f3672ba1c9ead9818febdb935f4fb
Clara Cunha <clara.cunha1@escolar.ifrn.edu.br>
2026-07-15 12:52:24 +0000

    Aula 15/07 - Parte 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,48 @@
-import './App.css';
-import Header from './components/Header';
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import NoticeList from "./components/NoticeList";
 
 function App() {
-  return (
-     <>
-      <Header />
-      <h1>Mural Digital do IFRN - Campus Macau</h1>
+  const [notices, setNotices] = useState([
+    {
+      id: 1,
+      title: "Secitex Macau 2026",
+      category: "Evento",
+      description: "Inscrições abertas para oficinas e palestras.",
+      author: "COINF/MC",
+      date: "01/07/2026",
+      featured: true,
+    },
+    {
+      id: 2,
+      title: "Manutenção do Lab. de Informática 2",
+      category: "Aviso",
+      description: "Indisponível na sexta 03/07/2026",
+      author: "CTI/MC",
+      date: "01/07/2026",
+      featured: false,
+    },
+  ]);
 
-      {notices.map(notice) -> (
-        <article>
-          <p>{notice.category}</p>
-        </article>
-      )
+  function handleToggleFeatured(id) {
+    setNotices((notices) =>
+      notices.map(
+        (notice) =>
+          notice.id === id ? { ...notice, featured: !notice.featured } : notice,
+      ),
+    );
+  }
+
+  return (
+    <>
+      <Header
+        title={"Programação para Internet"}
+        subtitle={"Curso Técnico Integrado em Informática"}
+      />
+      <NoticeList notices={notices} onToggleFeatured={handleToggleFeatured} />
     </>
   );
 }
 
 export default App;
-
-
